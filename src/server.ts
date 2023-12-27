225458336d3de7283dc26da084399960682889ed
arthdarji352 <arthdarji20@gmail.com>
2023-12-27 21:23:16 +0530

    test link staged3343

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ function login(username: string): string {
     // console.log(username);
     const user = {
         name: "arth",
-    }; const name = user["name"];
+    };
+    const name = user.name;
     return username + name;
 }
 
